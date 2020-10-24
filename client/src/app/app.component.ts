3a4400d3026fc5bce138dbf1bc2a8f878c4c03fa
VINCENT LABOURE <v.laboure@gmail.com>
2020-10-24 11:55:49 +0200

    premier Get depuis l'Api

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,20 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'skinet';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?itemsperpage=20').subscribe((response: any) => {
+      console.log(response); }, error => {console.log(error); });
+  }
 }
